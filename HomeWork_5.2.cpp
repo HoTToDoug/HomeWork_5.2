59fe27d9ba14383d19bc9b529a93dcd68ef7b8a1
HoTToDoug <hotto.doug@mail.ru>
2021-11-04 13:22:53 +0300

    Complete first work

--- HomeWork_5.2.cpp
+++ HomeWork_5.2.cpp
@@ -1,6 +1,31 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+template<class T>
+class Pair1 {
+private:
+    T a;
+    T b;
+public:
+    Pair1(T c, T d) : a(c), b(d) {};
+    T first() const {
+        return a;
+    };
+
+    T second() const {
+        return b;
+    };
+
+};
+
 int main()
 {
-    
+    Pair1<int> p1(6, 9);
+    cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+
+    const Pair1<double> p2(3.4, 7.8);
+    cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+
+    return 0;
 }
