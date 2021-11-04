5856281fc53da56c39f8b12cf550d69d08763805
HoTToDoug <hotto.doug@mail.ru>
2021-11-04 13:35:06 +0300

    Second work completed

--- HomeWork_5.2.cpp
+++ HomeWork_5.2.cpp
@@ -19,13 +19,43 @@ public:
 
 };
 
+template<class T, class Tem>
+class Pair {
+private:
+    T g;
+    Tem h;
+public:
+    Pair(T n, Tem m) : g(n), h(m) {};
+    T first() const {
+        return g;
+    };
+
+    Tem second() const {
+        return h;
+    };
+
+};
+
 int main()
 {
-    Pair1<int> p1(6, 9);
-    cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+    {
+        Pair1<int> p1(6, 9);
+        cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
-    const Pair1<double> p2(3.4, 7.8);
-    cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+        const Pair1<double> p2(3.4, 7.8);
+        cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+    };
+
+    cout << "/--------------------------------------------------/" << endl;
+
+    {
+        Pair<int, double> p1(6, 7.8);
+        cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+
+        const Pair<double, int> p2(3.4, 5);
+        cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+    };
+
 
     return 0;
 }
